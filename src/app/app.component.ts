edbdab5894f742a53a584c315460a0fb6d2c0008
Max <bilousmaks27@gmail.com>
2024-10-22 14:10:27 +0300

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {
+  HttpClient,
+  HttpClientModule
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [HttpClientModule, CommonModule]
 })
-export class AppComponent {
-  title = 'WeatherSense';
+export class AppComponent implements OnInit {
+  weather: any;
+
+  constructor(private http: HttpClient) {}
+  private apiKey = '746a88c4a60ad969a5a50b35e4f32082';
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  ngOnInit(): void {
+    this.getWeather('Kiev').subscribe((data) => {
+      this.weather = data;
+      console.log(data);
+    });
+  }
+
+  
+  getWeather(city: string): Observable<any> {
+    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`;
+    return this.http.get(url);
+  }
 }
---
+++ src/app/weather.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey = '746a88c4a60ad969a5a50b35e4f32082'; // Встав свій API ключ
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  constructor(private http: HttpClient) {}
+
+  getWeather(city: string): Observable<any> {
+    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`;
+    return this.http.get(url);
+  }
+}
